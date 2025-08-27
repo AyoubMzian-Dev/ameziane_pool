2dca31c54e5e4478cdbe041a686fc4009ea8c989
AyoubMzian-Dev <ayoubmziandeveloper@gmail.com>
2025-08-27 23:00:46 +0100

    progress

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,4 +1,7 @@
+#include <unistd.h>
+#include <stdio.h>
 
+// This function is still not correct for multi-digit numbers
 void	print_nbr(int nb)
 {
 	char	str = nb + '0';
@@ -6,7 +9,8 @@ void	print_nbr(int nb)
 	write(1, &str, 1);
 }
 
-void	ft_atio(char *str)
+int
+ft_atoi(char *str)
 {
 	int	i;
 	int	sign;
@@ -17,20 +21,43 @@ void	ft_atio(char *str)
 	num = 0;
 	while(str[i] != '\0')
 	{
-		if (str[i] == '-')
+		if (str[i] <= ' ')
 		{
-			sign = sign * -1;
+			if (num > 0)
+				return (num * sign);
+			i++;
+			continue;
 		}
-		else if (str[i] == '+')
+		else if (str[i] == '-' || str[i] == '+' || str[i] == ' ')
 		{
-			sign = sign * 1;
+			if (str[i] == '-')
+				sign = sign * -1;
 		}
 		else if (str[i] >= '0' && str[i] <= '9')
 		{
-			num = num + (str[i] - '0');
+			num = num * 10 + (str[i] - '0');
 		}
-		else if (str[i] < '0' || str[i] > '9')
-		{
+		else
 			return (num * sign);
+		i++;
+	}
 	return (num * sign);
 }
+
+/*int main(void)
+{
+    printf("Test [18] (Expected 12): %d\n", ft_atoi("12 4"));
+    printf("Test [19] (Expected 12): %d\n", ft_atoi("  +12 4"));
+    printf("--------------------------\n");
+    printf("Testing with \"42\": %d\n", ft_atoi("42"));
+    printf("Testing with \"-42\": %d\n", ft_atoi("-42"));
+    printf("Testing with \"   +42\": %d\n", ft_atoi("   +42"));
+    printf("Testing with \"  --42\": %d\n", ft_atoi("  --42"));
+    printf("Testing with \"42abc55\": %d\n", ft_atoi("42abc55"));
+    printf("Testing with \"0\": %d\n", ft_atoi("0"));
+    printf("Testing with an empty string \"\": %d\n", ft_atoi(""));
+    printf("Testing with \"  \t\n\": %d\n", ft_atoi("  \t\n"));
+    printf("Testing with \"1-2\": %d\n", ft_atoi("1-2"));
+
+    return (0);
+}*/
